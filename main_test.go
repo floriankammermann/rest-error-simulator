cd192043d8c0146cf17b8d03ebb87ef013ca9fd1
floriankammermann <florian@kammermann.me>
2020-10-29 12:11:12 +0100

    added first simple implementation of errorcode ratio

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestRatio(t *testing.T) {
+	responseCode := getResponseCode(1, 1, 200, 500)
+	if responseCode != 200 {
+		t.Fail()
+	}
+	responseCode = getResponseCode(3, 2, 200, 500)
+	if responseCode != 500 {
+		t.Fail()
+	}
+
+}
