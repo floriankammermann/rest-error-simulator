137274566f0d786a87e9bcbea8f6aa922fbc5679
taagaar1 <arthur.gassmann@swisscom.com>
2020-11-05 08:06:00 +0100

    add errorratio + latency api

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/floriankammermann/rest-error-simulator
 
 go 1.15
 
-require (
-	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/prometheus/client_golang v1.8.0
-)
+require google.golang.org/grpc v1.26.0
--- main.go
+++ main.go
@@ -1,49 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"google.golang.org/grpc/benchmark/latency"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"strconv"
-
-	"github.com/kelseyhightower/envconfig"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"time"
 )
 
-var (
-	ResponseCodeInternalServerError = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "response_internal_server_error",
-		Help: "amount of internal server errors",
-	})
-	ResponseCodeStatusOK = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "response_status_ok",
-		Help: "amount of status ok",
-	})
-)
-
-type Specification struct {
-	ResponseCodeSuccess                   int
-	ResponseCodeFailure                   int
-	ResponseCodeSuccessFailureRatio       int
-	ResponseCodeSuccessFailureRatioModulo int
+// bufConn is a net.Conn implemented by a bytes.Buffer (which is a ReadWriter).
+type bufConn struct {
+	*bytes.Buffer
 }
 
-func (s *Specification) init() {
-	if s.ResponseCodeSuccess == 0 {
-		s.ResponseCodeSuccess = 200
-	}
-	if s.ResponseCodeFailure == 0 {
-		s.ResponseCodeFailure = 500
-	}
-	if s.ResponseCodeSuccessFailureRatio == 0 {
-		s.ResponseCodeSuccessFailureRatioModulo = 1
-	}
-	if s.ResponseCodeSuccessFailureRatio == 50 {
-		s.ResponseCodeSuccessFailureRatioModulo = 2
-	}
-}
+func (bufConn) Close() error                       { panic("unimplemented") }
+func (bufConn) LocalAddr() net.Addr                { panic("unimplemented") }
+func (bufConn) RemoteAddr() net.Addr               { panic("unimplemented") }
+func (bufConn) SetDeadline(t time.Time) error      { panic("unimplemneted") }
+func (bufConn) SetReadDeadline(t time.Time) error  { panic("unimplemneted") }
+func (bufConn) SetWriteDeadline(t time.Time) error { panic("unimplemneted") }
 
 func getResponseCode(requestCounter, ratio, successCode, errorCode int) int {
 	rest := requestCounter % ratio
@@ -60,57 +38,120 @@ func setRestRatio(errorratioInt int) int {
 }
 
 func main() {
-	var s Specification
-	err := envconfig.Process("res", &s)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	s.init()
 
+	var responseCode = http.StatusOK
+	var responseCodeSuccess = http.StatusOK
+	var ratio = 1
 	var requestCounter = 1
+	var lty = 10
 
 	bestTools := func(w http.ResponseWriter, req *http.Request) {
-		rest := requestCounter % s.ResponseCodeSuccessFailureRatioModulo
+		rest := requestCounter % ratio
 		if rest != 0 {
-			w.WriteHeader(s.ResponseCodeSuccess)
-			ResponseCodeInternalServerError.Inc()
+			w.WriteHeader(responseCodeSuccess)
 		} else {
-			w.WriteHeader(s.ResponseCodeFailure)
-			ResponseCodeStatusOK.Inc()
+			w.WriteHeader(responseCode)
 		}
 		w.Header().Add("Content-Type", "application/json")
 		io.WriteString(w, `{"bestTools":{"cidcd": "Jenkins"}}`)
 		requestCounter++
 		log.Printf("requestCounter: %d", requestCounter)
-		log.Printf("ratio: %d", s.ResponseCodeSuccessFailureRatio)
+		log.Printf("ratio: %d", ratio)
 	}
 
 	introduceHttpErrorCodes := func(w http.ResponseWriter, req *http.Request) {
 		errorcode := req.URL.Query()["errorcode"]
-		errorratio := req.URL.Query()["errorratio"]
+		if len(errorcode) == 0 {
+			return
+		}
+		errorcodeInt, err := strconv.Atoi(errorcode[0])
+		if err != nil {
+			log.Printf("errorcode is not a number: %s", errorcode)
+		}
+		log.Printf("set errorcode to %s", errorcode)
+		responseCode = errorcodeInt
 
-		if len(errorcode) != 0 {
-			errorcodeInt, err := strconv.Atoi(errorcode[0])
-			if err != nil {
-				log.Printf("errorcode is not a number: %s", errorcode)
-			}
-			s.ResponseCodeFailure = errorcodeInt
-			log.Printf("set ResponseCode to %d", s.ResponseCodeFailure)
+		errorratio := req.URL.Query()["errorratio"]
+		if len(errorratio) == 0 {
+			return
 		}
-		if len(errorratio) != 0 {
-			errorratioInt, err := strconv.Atoi(errorratio[0])
-			if err != nil {
-				log.Printf("errorratio is not a number: %s", errorratio)
-			}
-			s.ResponseCodeSuccessFailureRatio = setRestRatio(errorratioInt
-			logResponse := "set erroratio to " + strconv.Itoa(ratio) + " (" + strconv.Itoa(errorratioInt) + "%)"
-			log.Println(logResponse)
+		errorratioInt, err := strconv.Atoi(errorratio[0])
+		if err != nil {
+			log.Printf("errorratio is not a number: %s", errorratio)
 		}
+		/*if errorratioInt == 50 {
+			log.Println("set errorratio to 2 (50%)")
+			ratio = 2
+		} else if errorratioInt == 20 {
+			log.Println("set errorratio to 5 (20%)")
+			ratio = 5
+		} else if errorratioInt == 33 {
+			log.Println("set errorratio to 3 (30%)")
+			ratio = 3
+		}*/
+		ratio = setRestRatio(errorratioInt)
+		logResponse := "set erroratio to " + strconv.Itoa(ratio) + " (" + strconv.Itoa(errorratioInt) + "%)"
+		log.Println(logResponse)
+
+	}
+
+	bestApps := func(w http.ResponseWriter, req *http.Request) {
+		var i = time.Duration(lty)
+		slowConn, err := (&latency.Network{Kbps: 0, Latency: i * time.Millisecond, MTU: 5}).Conn(bufConn{&bytes.Buffer{}})
+		if err != nil {
+			log.Printf("Unexpected error creating connection: %v", err)
+		}
+
+		errWant := "measured network latency (10ms) higher than desired latency (5ms)"
+		if _, err := (&latency.Network{Latency: 5 * time.Millisecond}).Conn(slowConn); err == nil || err.Error() != errWant {
+			log.Printf("Conn() = _, %q; want _, %q", err, errWant)
+		}
+
+		/*n := &latency.Network{Kbps: 0, Latency: 1 * time.Millisecond, MTU: 5}
+		n.Dialer (net.Dial) ("tcp", "localhost:8000")
+		//clientConn, err := n.Conn(d)
+		*/
+
+		w.Header().Add("Content-Type", "application/json")
+		io.WriteString(w, `{"bestApps":{"devOps": "DevOps"}}`)
+	}
+
+	introduceLatency := func(w http.ResponseWriter, req *http.Request) {
+		ms := req.URL.Query()["ms"]
+		if len(ms) == 0 {
+			return
+		}
+		latencyInt, err := strconv.Atoi(ms[0])
+		if err != nil {
+			log.Printf("latency is not a number: %s", ms)
+		}
+		lty = latencyInt
+		log.Printf("latency is set to %d ms", lty)
+
+		// Infinitely fast CPU: time doesn't pass unless sleep is called.
+		//tn := time.Unix(123, 0)
+		//now := func() time.Time { return tn }
+		//sleep := func(d time.Duration) { tn = tn.Add(d) }
+
+		// Simulate a 10ms latency network, then attempt to simulate a 5ms latency
+		// network and expect an error.
+		/*
+			slowConn, err := (&latency.Network{Kbps: 0, Latency: 100 * time.Millisecond, MTU: 5}).Conn(bufConn{&bytes.Buffer{}})
+			if err != nil {
+				log.Printf("Unexpected error creating connection: %v", err)
+			}
+
+			errWant := "measured network latency (10ms) higher than desired latency (5ms)"
+			if _, err := (&latency.Network{Latency: 5 * time.Millisecond}).Conn(slowConn); err == nil || err.Error() != errWant {
+				log.Printf("Conn() = _, %q; want _, %q", err, errWant)
+			}
+		*/
 	}
 
 	http.HandleFunc("/best-tools", bestTools)
+	http.HandleFunc("/best-apps", bestApps)
 	http.HandleFunc("/control/error", introduceHttpErrorCodes)
-	http.Handle("/metrics", promhttp.Handler())
-	log.Println("Listening for requests at http://localhost:8080/best-tools")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/control/latency", introduceLatency)
+	log.Println("Listing for requests at http://localhost:8000/best-tools")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
