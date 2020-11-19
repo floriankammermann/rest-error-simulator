189756841157f0a547594506c059df3b8bf42e3c
floriankammermann <florian@kammermann.me>
2020-11-19 08:28:09 +0100

    added host as env and set to localhost:8080

--- rest-error-simulator-server-test.sh
+++ rest-error-simulator-server-test.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
-
-curl localhost:8080/control
+: ${host:=localhost:8080}
+echo "use host: $host"
+curl $host/control
 echo ""
 echo "*********************"
 echo "set error ratio to 1"
 echo "*********************"
-curl localhost:8080/control/error?errorratio=1 -X POST
-curl localhost:8080/control
+curl $host/control/error?errorratio=1 -X POST
+curl $host/control
 echo ""
 echo "*********************"
 echo "get best-tools 3 times" 
 echo "*********************"
 for i in {1..3}; do
-    curl -Is localhost:8080/best-tools | head -1 | cut -d " " -f2 && curl -s localhost:8080/metrics | egrep "^response.*"
+    curl -Is $host/best-tools | head -1 | cut -d " " -f2 && curl -s $host/metrics | egrep "^response.*"
 done
 echo "*********************"
 echo "set error ratio to 100"
 echo "*********************"
-curl localhost:8080/control/error?errorratio=100 -X POST
-curl localhost:8080/control
+curl $host/control/error?errorratio=100 -X POST
+curl $host/control
 echo ""
 echo "*********************"
 echo "get best-tools 3 times" 
 echo "*********************"
 for i in {1..3}; do
-    curl -Is localhost:8080/best-tools | head -1 | cut -d " " -f2 && curl -s localhost:8080/metrics | egrep "^response.*"
+    curl -Is $host/best-tools | head -1 | cut -d " " -f2 && curl -s $host/metrics | egrep "^response.*"
 done
 echo "*********************"
 echo "set error ratio to 50"
 echo "*********************"
-curl localhost:8080/control/error?errorratio=50 -X POST
-curl localhost:8080/control
+curl $host/control/error?errorratio=50 -X POST
+curl $host/control
 echo ""
 echo "*********************"
 echo "get best-tools 6 times" 
 echo "*********************"
 for i in {1..6}; do 
-    curl -Is localhost:8080/best-tools | head -1 | cut -d " " -f2 && curl -s localhost:8080/metrics | egrep "^response.*"
+    curl -Is $host/best-tools | head -1 | cut -d " " -f2 && curl -s $host/metrics | egrep "^response.*"
 done
 
 
